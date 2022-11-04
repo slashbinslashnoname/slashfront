cadbe1422fc69da14943ead69da68a4e52f68ef0
Slashbinslashnoname <slashbinslashnoname@gmail.com>
2022-11-04 18:38:41 +0100

    Process Fix

--- src/components/pages/Process/Process.js
+++ src/components/pages/Process/Process.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React from 'react'
 import {
-  Container, Grid, Typography, Divider, Button, Paper,
-} from '@mui/material';
+  Container,
+  Grid,
+  Typography,
+  Divider,
+  Button,
+  Paper,
+} from '@mui/material'
 
 import {
-  ArrowOutward, AutoStories,
-  Architecture, VideoCameraFront,
-  AccessibilityNew, NoteAdd, Code, AttachMoney,
-} from '@mui/icons-material';
+  ArrowOutward,
+  AutoStories,
+  Architecture,
+  VideoCameraFront,
+  AccessibilityNew,
+  NoteAdd,
+  Code,
+  AttachMoney,
+} from '@mui/icons-material'
 
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-import RepeatIcon from '@mui/icons-material/Repeat';
-import RequestQuote from '@mui/icons-material/RequestQuote';
+import RepeatIcon from '@mui/icons-material/Repeat'
+import RequestQuote from '@mui/icons-material/RequestQuote'
 import {
-  Timeline, TimelineItem, TimelineContent,
+  Timeline,
+  TimelineItem,
+  TimelineContent,
   TimelineConnector,
-  TimelineDot, TimelineSeparator,
-} from '@mui/lab';
+  TimelineDot,
+  TimelineSeparator,
+} from '@mui/lab'
 
 import TimelineOppositeContent, {
   timelineOppositeContentClasses,
-} from '@mui/lab/TimelineOppositeContent';
+} from '@mui/lab/TimelineOppositeContent'
 
 function Process() {
   return (
@@ -40,50 +53,38 @@ function Process() {
               </Typography>
             </Grid>
           </Grid>
-
         </Container>
       </Grid>
 
       <Container className="container-body">
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          justify="center"
-        >
+        <Grid container direction="column" alignItems="center" justify="center">
           <Grid item xs={12} md={12}>
             <Typography variant="h3" component="h3">
               You are a project builder
             </Typography>
-
           </Grid>
           <Grid item xs={12} md={12}>
             <p>
-              Size doesn&apos;t matter ! We accept very small projects as well as large projects.
+              Size doesn&apos;t matter ! We accept very small projects as well
+              as large projects.
             </p>
           </Grid>
           <Grid item xs={12} md={12}>
-
             <p>
-              We accompany you to build your Bitcoin or
-              Web3 project from the birth of your idea
-              to its realization through
-              a feasibility study.
+              We accompany you to build your Bitcoin or Web3 project from the
+              birth of your idea to its realization through a feasibility study.
             </p>
           </Grid>
-
         </Grid>
 
-        <Grid container>
+        <Grid container sx={{ padding: '4em' }}>
           <Paper elevation={2}>
-
             <Grid
               container
               direction="column"
               alignItems="center"
               justify="center"
             >
-
               <Grid item xs={12} md={12}>
                 <Typography variant="h3" component="h3">
                   Our process
@@ -98,12 +99,18 @@ function Process() {
               </Grid>
             </Grid>
 
-            <Grid item xs={12} md={12}>
-              <Timeline sx={{
-                [`& .${timelineOppositeContentClasses.root}`]: {
-                  flex: 0.2,
-                },
-              }}
+            <Grid
+              item
+              xs={12}
+              md={12}
+              sx={{ paddingBottom: '4em', paddingRight: '5em' }}
+            >
+              <Timeline
+                sx={{
+                  [`& .${timelineOppositeContentClasses.root}`]: {
+                    flex: 0.2,
+                  },
+                }}
               >
                 <TimelineItem>
                   <TimelineOppositeContent
@@ -152,9 +159,9 @@ function Process() {
                       Skills Repartition
                     </Typography>
                     <Typography>
-                      We create a channel inside our private freelancers
-                      space to concentrate all the useful
-                      informations about your project.
+                      We create a channel inside our private freelancers space
+                      to concentrate all the useful informations about your
+                      project.
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
@@ -179,9 +186,8 @@ function Process() {
                       Communication with the team
                     </Typography>
                     <Typography>
-                      We create a channel for you and our
-                      freelancers to communicate freely
-                      without barriers.
+                      We create a channel for you and our freelancers to
+                      communicate freely without barriers.
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
@@ -205,7 +211,9 @@ function Process() {
                     <Typography variant="h6" component="span">
                       Project Architecture
                     </Typography>
-                    <Typography>We design your global project architecture</Typography>
+                    <Typography>
+                      We design your global project architecture
+                    </Typography>
                   </TimelineContent>
                 </TimelineItem>
 
@@ -229,10 +237,9 @@ function Process() {
                       Sprint design
                     </Typography>
                     <Typography>
-                      Freelancers build tickets to work on.
-                      They choose the priority.
-                      You choose which are assigned for the next sprint
-                      in accordance with the global roadmap.
+                      Freelancers build tickets to work on. They choose the
+                      priority. You choose which are assigned for the next
+                      sprint in accordance with the global roadmap.
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
@@ -257,7 +264,8 @@ function Process() {
                       Quotes
                     </Typography>
                     <Typography>
-                      Freelancers sends you their quotes. You are free to choose them.
+                      Freelancers sends you their quotes. You are free to choose
+                      them.
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
@@ -282,7 +290,8 @@ function Process() {
                       Development
                     </Typography>
                     <Typography>
-                      Freelancers code and delivers until the sprint is finished.
+                      Freelancers code and delivers until the sprint is
+                      finished.
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
@@ -330,8 +339,9 @@ function Process() {
                       Repeat to step 5
                     </Typography>
                     <Typography>
-                      Until the project is over or you don&apos;t need our services anymore.
-                      Because this is how we try to build a better future !
+                      Until the project is over or you don&apos;t need our
+                      services anymore. Because this is how we try to build a
+                      better future !
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
@@ -358,7 +368,6 @@ function Process() {
                   See our culture
                 </Button>
               </Link>
-
             </Grid>
           </Grid>
           <Grid item xs={12} md={6}>
@@ -381,10 +390,9 @@ function Process() {
             </Grid>
           </Grid>
         </Grid>
-
       </Container>
     </>
-  );
+  )
 }
 
-export default Process;
+export default Process
