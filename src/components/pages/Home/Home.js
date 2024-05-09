5dcd14b0823ec2d9af21c6cfc88c8814365ab89d
Slashbin <slashbinslashnoname@gmail.com>
2024-05-09 13:24:06 +0200

    Remove pages

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,174 +1,182 @@
-import React, { useCallback } from 'react';
-import {
-  Container, Grid, Button, Typography,
-} from '@mui/material';
+import React, { useCallback } from 'react'
+import { Container, Grid, Button, Typography } from '@mui/material'
 
-import Particles from 'react-particles';
-import { loadFull } from 'tsparticles';
+import Particles from 'react-particles'
+import { loadFull } from 'tsparticles'
 
-import { ArrowOutward, Memory } from '@mui/icons-material';
+import { ArrowOutward, Memory } from '@mui/icons-material'
 
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-const ReactRotatingText = require('react-rotating-text');
+const ReactRotatingText = require('react-rotating-text')
 
 function Home({ media }) {
-  require('pathseg');
+  require('pathseg')
 
   const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine);
-  }, []);
+    await loadFull(engine)
+  }, [])
 
   const particlesLoaded = useCallback(async () => {
-  /* added code start */
-
-  }, []);
+    /* added code start */
+  }, [])
 
   return (
     <>
       {!media.md && (
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-
-          fullScreen: {
-            enable: false,
-          },
-          detectRetina: false,
-          fpsLimit: 24,
-          interactivity: {
-            detectsOn: 'window',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'grab',
-                parallax: {
-                  enable: false,
-                  force: 6,
-                  smooth: 6,
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fullScreen: {
+              enable: false,
+            },
+            detectRetina: false,
+            fpsLimit: 24,
+            interactivity: {
+              detectsOn: 'window',
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'grab',
+                  parallax: {
+                    enable: false,
+                    force: 6,
+                    smooth: 6,
+                  },
                 },
+                resize: false,
               },
-              resize: false,
             },
-          },
-          particles: {
-            color: {
-              value: '#ffffff',
-            },
-            links: {
+            particles: {
               color: {
                 value: '#ffffff',
               },
-              distance: 70,
-              enable: true,
-              opacity: 0.4,
-            },
-            move: {
-              noise: {
-                enable: false,
+              links: {
+                color: {
+                  value: '#ffffff',
+                },
+                distance: 70,
+                enable: true,
+                opacity: 0.4,
               },
-              attract: {
-                rotate: {
-                  x: 150,
-                  y: 350,
+              move: {
+                noise: {
+                  enable: false,
+                },
+                attract: {
+                  rotate: {
+                    x: 150,
+                    y: 350,
+                  },
+                },
+                enable: true,
+                path: {},
+                outModes: {
+                  default: 'split',
+                  bottom: 'split',
+                  left: 'split',
+                  right: 'split',
+                  top: 'split',
+                },
+                speed: 0.3,
+                spin: {},
+              },
+              number: {
+                density: {
+                  area: 200,
+                },
+                value: 200,
+              },
+              opacity: {
+                value: {
+                  min: 0.05,
+                  max: 0.4,
+                },
+                animation: {
+                  enable: false,
+                  minimumValue: 0.05,
                 },
               },
-              enable: true,
-              path: {},
-              outModes: {
-                default: 'split',
-                bottom: 'split',
-                left: 'split',
-                right: 'split',
-                top: 'split',
-              },
-              speed: 0.3,
-              spin: {},
-            },
-            number: {
-              density: {
-                area: 200,
-              },
-              value: 200,
-            },
-            opacity: {
-              value: {
-                min: 0.05,
-                max: 0.4,
-              },
-              animation: {
-                enable: false,
-                minimumValue: 0.05,
+              size: {
+                random: {
+                  enable: true,
+                },
+                value: 1,
+                animation: {
+                  speed: 10,
+                  minimumValue: 0.1,
+                },
               },
             },
-            size: {
-              random: {
+
+            polygon: {
+              draw: {
                 enable: true,
+                strokeWidth: 1,
               },
-              value: 1,
-              animation: {
-                speed: 10,
-                minimumValue: 0.1,
-              },
-            },
-          },
-
-          polygon: {
-            draw: {
               enable: true,
-              strokeWidth: 1,
+              inline: {
+                arrangement: 'equidistant',
+              },
+              move: {
+                radius: 3,
+                type: 'path',
+              },
+              scale: 0.4,
+              type: 'inline',
+              url: '/images/logo.svg',
             },
-            enable: true,
-            inline: {
-              arrangement: 'equidistant',
-            },
-            move: {
-              radius: 3,
-              type: 'path',
-            },
-            scale: 0.4,
-            type: 'inline',
-            url: '/images/logo.svg',
-          },
-
-        }}
-      />
+          }}
+        />
       )}
 
-      <Grid className="container container-head container-home" sx={{ background: 'none' }}>
-
+      <Grid
+        className="container container-head container-home"
+        sx={{ background: 'none' }}
+      >
         <Container>
           <Grid container>
             <Grid container>
               <Grid item xs={12} md={12}>
                 <h1>
-                  We have
-                  {' '}
-                  <ReactRotatingText items={['Bitcoin', 'Ethereum', 'Dev', 'Nodes', 'Consensus', 'Smartcontracts']} />
-                  {' '}
+                  We have{' '}
+                  <ReactRotatingText
+                    items={[
+                      'Bitcoin',
+                      'Ethereum',
+                      'Dev',
+                      'Nodes',
+                      'Consensus',
+                      'Smartcontracts',
+                    ]}
+                  />{' '}
                   <br />
                   Experiences.
                 </h1>
               </Grid>
 
-              <Grid item xs={12} md={12} sx={{ position: 'absolute', bottom: 250 }}>
+              <Grid
+                item
+                xs={12}
+                md={12}
+                sx={{ position: 'absolute', bottom: 250 }}
+              >
                 <Typography variant="h6" sx={{ fontWeight: '600' }}>
-                  Slashbin.xyz is a decentralized autonomous guild of freelancers and OGs
-                  developers.
+                  Slashbin.xyz is a decentralized autonomous guild of
+                  freelancers and OGs developers.
                 </Typography>
 
-                <Typography variant="h4" sx={{ fontWeight: '600' }}>
-                  We build your project from A to Z for Bitcoin and Crypto projects.
-                </Typography>
-
+                {!media.md && (
+                  <Typography variant="h4" sx={{ fontWeight: '600' }}>
+                    We build your project from A to Z for Bitcoin and Crypto
+                    projects.
+                  </Typography>
+                )}
               </Grid>
-
             </Grid>
-
           </Grid>
-
         </Container>
         <Grid
           container
@@ -179,19 +187,6 @@ function Home({ media }) {
         >
           <Container>
             <Grid container>
-
-              <Grid item xs={12} md={3} sx={media.md && { marginBottom: '2em' }}>
-                <Link to="/process">
-                  <Button
-                    variant="contained"
-                    size="large"
-                    color="secondary"
-                    startIcon={<Memory />}
-                  >
-                    See our process
-                  </Button>
-                </Link>
-              </Grid>
               <Grid item xs={12} md={3}>
                 <Link to="/contact">
                   <Button
@@ -211,7 +206,7 @@ function Home({ media }) {
         </Grid>
       </Grid>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
